3377ddcb2386128dfd492d0be89b0a29d909c78e
menkooky <dawahmenna@std.mans.edu.eg>
2023-09-25 21:40:50 +0300

    18/9

--- lib/features/screens/login/enter_otp_screen.dart
+++ lib/features/screens/login/enter_otp_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 import 'package:joboard1/core/app_colors.dart';
+import 'package:joboard1/widgets/app_button.dart';
 import 'package:joboard1/widgets/app_text.dart';
 
 class EnterOtp extends StatefulWidget {
@@ -19,45 +22,62 @@ class _EnterOtpState extends State<EnterOtp> {
         elevation: 0,
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 70),
-          child: Column(
-            children: [
-              Image(
-                image: AssetImage("assets/images/digit.png"),
-                width: 200,
-                height: 270,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 40),
-              AppText(
-                title: "Enter OTP",
-                color: AppColors.black,
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-              ),
-              SizedBox(height: 32),
-              AppText(
-                title: "An 6 digits code has been sent to +20  15849663591",
-                color: AppColors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
-              ),
-              SizedBox(height: 32,),
-              Container(
-                width: 34,
-                height: 34,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
-                child: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: AppColors.black,
-                    border: InputBorder.none,
-                  ),
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage("assets/images/digit.png"),
+              width: 200,
+              height: 270,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 40),
+            AppText(
+              title: "Enter OTP",
+              color: AppColors.black,
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            SizedBox(height: 32),
+            Padding(
+              padding: const EdgeInsets.only(left: 35),
+              child: Container(
+                width: 260,
+                child: AppText(
+                  title: "An 6 digits code has been sent to +20  15849663591",
+                  color: AppColors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
                 ),
               ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            SizedBox(
+              height: 34,
+              child: OtpTextField(
+                fieldWidth: 34,
+                numberOfFields: 6,
+                fillColor: Colors.black.withOpacity(0.1),
+                filled: true,
+              ),
+            ),
+            SizedBox(height: 40),
+            Container(
+              width: 208,
+              height: 37,
+              color: AppColors.blue,
+              child: Center(
+                  child: Text(
+                "Submit",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  color: AppColors.white,
+                ),
+              )),
+            ),
+          ],
         ),
       ),
     );
